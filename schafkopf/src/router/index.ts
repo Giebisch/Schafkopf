3f9d19f1215de9d7fb2f18b9f8b7be3dd5dab3ce
Giebisch <rafael@giebisch-mail.de>
2022-03-27 18:28:12 +0200

    Initial Setup

---
+++ schafkopf/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '',
+    redirect: '/Spiele'
+  },
+  {
+    path: '/Spiele',
+    component: () => import ('../views/FolderPage.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router

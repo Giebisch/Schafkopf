89a2bb9fa85288813fe971ef0e399223567bea26
Giebisch <giebisch@mobile-software.ag>
2022-04-26 19:06:53 +0200

    update

---
+++ schafkopf/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.rafael-giebisch',
+  appName: 'Schafkopf',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
